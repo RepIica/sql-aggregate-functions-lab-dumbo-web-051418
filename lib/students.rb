9d2134e309394ffdf25344ca7bf50b3e75d1ae9f
Sammy Akharaz <ketrixz@gmail.com>
2018-05-21 20:49:09 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,19 +5,19 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  "SELECT gpa FROM students ORDER BY gpa ASC LIMIT 1"
+  "SELECT gpa FROM students ORDER BY gpa ASC LIMIT 1;"
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM students"
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) FROM"
+  "SELECT SUM(tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade
-  "SELECT AVG(gpa) FROM grade = 9"
+  "SELECT AVG(gpa) FROM grade = 9;"
 end
 
 # INSERT INTO students (name, grade, gpa, tardies)
