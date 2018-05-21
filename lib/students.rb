1d1204fe57c9d39960c32ddf0e8b244edf288efa
Sammy Akharaz <ketrixz@gmail.com>
2018-05-21 20:45:45 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,11 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  "SELECT FROM"
+  "SELECT gpa FROM students ORDER BY gpa ASC LIMIT 1"
 end
 
 def average_student_gpa
-  "SELECT FROM"
+  "SELECT AVG FROM"
 end
 
 def total_tardies_for_all_students
