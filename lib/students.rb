f5e0668553a9e05d6983e1072f5e0229f6ce5e4a
Sammy Akharaz <ketrixz@gmail.com>
2018-05-21 20:41:45 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,21 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
+  "SELECT FROM"
 end
 
 def lowest_student_gpa
+  "SELECT FROM"
 end
 
 def average_student_gpa
+  "SELECT FROM"
 end
 
 def total_tardies_for_all_students
+  "SELECT FROM"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT FROM"
 end
